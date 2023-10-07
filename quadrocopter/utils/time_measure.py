b123db6a009ada051d80366626a625367f16a5b4
damjab <damianjablczyk@gmail.com>
2023-10-07 21:40:55 +0200

    update time_measure

--- quadrocopter/utils/time_measure.py
+++ quadrocopter/utils/time_measure.py
@@ -1,37 +1,41 @@
 import random
 import time
 
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 from quadrocopter.model.path_finder import PathFinder
 from quadrocopter.model.utils import Transmitter, Point
 
 # Constants
-MAX_TRANSMITTERS = 10000
+MAX_TRANSMITTERS = 1000
 ITERATIONS = 100
 TIMES = []
 
 
 # Function to run path finding with random transmitters
 def run_path_finding(transmitters_count: int) -> float:
+    max_range = transmitters_count
+    max_power = max(int(transmitters_count / 14), 1)
     _transmitters = [
-        Transmitter(Point(random.randint(0, 100), random.randint(0, 100)), random.randint(0, 100)) for _ in
+        Transmitter(Point(random.randint(0, max_range), random.randint(0, max_range)), random.randint(1, max_power)) for
+        _ in
         range(transmitters_count)]
-    _start = Point(random.randint(0, 100), random.randint(0, 100))
-    _end = Point(random.randint(0, 100), random.randint(0, 100))
+    _start = random.sample(_transmitters, k=1)[0].center
+    _end = random.sample(_transmitters, k=1)[0].center
     tp = PathFinder(start=_start, end=_end, transmitters=_transmitters)
     start_time = time.time()
-    tp.is_path_possible()
+    _, path = tp.is_path_possible()
     elapsed_time = time.time() - start_time
     return elapsed_time
 
 
 # Create data for different transmitter counts
-transmitter_counts = range(1, MAX_TRANSMITTERS + 1, 10)
+transmitter_counts = range(10, MAX_TRANSMITTERS + 1, 10)
 average_times = []
 
-for transmitters_count in transmitter_counts:
+for transmitters_count in tqdm(transmitter_counts):
     iteration_times = []
     for _ in range(ITERATIONS):
         elapsed_time = run_path_finding(transmitters_count)
